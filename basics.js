2c1da181007037bc14f47a8e212db0e05956ed79
alonzogravel92782-png <alonzogravel92782@gmail.com>
2025-10-29 16:22:11 +0200

    Javascript

---
+++ basics.js
@@ -0,0 +1,13 @@
+// TODO: Create a variable called firstName and store your first name in it 
+const firstName = "Alonzo"
+// TODO: Create a variable called lastName and store your last name in it
+const lastName = "Grave" 
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = 23
+// TODO: Create a fullName variable that combines firstName and lastName
+const fullName = "Alonzo Gravel"
+// TODO: Print a greeting to the console using these variables
+console.log(`Greetings ${firstName} ${lastName}, who is ${age}`)
+// TODO: Try changing the age variable to a different value
+age = 24
+console.log(`changed age: ${age}`)
